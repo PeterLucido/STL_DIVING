228fb854a9f07027f38f2c08550ab4afcd10bd44
Peter Lucido <127632779+PeterLucido@users.noreply.github.com>
2026-06-17 19:10:37 -0500

    Simplify footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,12 +1,9 @@
 export default function Footer() {
   return (
     <footer className="footer">
-      <div className="wrap">
-        <div>
-          <b>STL Diving</b>
-          <div className="small">Springboard - Platform - Olympic-style diving development · <a href="tel:+16367952482">(636) 795-2482</a></div>
-        </div>
-        <div className="small">2026 STL Diving. All rights reserved. · <a href="/photo-credits">Photo credits</a></div>
+      <div className="wrap footer-simple">
+        <b>STL Diving</b>
+        <a className="small" href="/photo-credits">Photo credits</a>
       </div>
     </footer>
   );
